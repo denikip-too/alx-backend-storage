12c374cacce2f1a08482715338b9c3625b785244
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-07-03 02:29:28 -0700

    Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,6 +2,5 @@
 CREATE TRIGGER v_email BEFORE INSERT ON email
 FOR EACH ROW
 	BEGIN
-		UPDATE users SET email=New.email;
-		IF (New.email REGEXP '^[^@]+@[^@]+\.[^@]{2,}$') THEN SET valid_email=0;
+		UPDATE users SET email=New.email IF (New.email REGEXP '^[^@]+@[^@]+\.[^@]{2,}$') THEN SET New.valid_email=0;
 	END;
