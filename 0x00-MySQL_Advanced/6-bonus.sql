63801b1a00a504f892964bf7c2a099fb34b7bd09
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-11 20:31:22 +0000

    MySQL

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE AddBonus(IN users_id INT, IN project_name VARCHAR(15), IN score FLOAT)
 BEGIN
 	INSERT INTO projects (name) VALUES (project_name);
-	INSERT INTO correction (users.id, project_name, score) VALUES (users.id, project_name, score);
+	INSERT INTO correction (users_id, project_name, score) VALUES (users_id, project_name, score);
 END
 $$
 DELIMiTER ;
