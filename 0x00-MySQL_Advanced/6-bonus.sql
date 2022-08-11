33168a428d8117f46992b6cf6d10dcf380819ce9
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-11 21:33:20 +0000

    MySQL

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,8 +2,14 @@
 DELIMITER $$
 CREATE PROCEDURE AddBonus(IN users_id INT, IN project_name VARCHAR(15), IN score FLOAT)
 BEGIN
+	DECLARE project_id INT;
+	IF NOT EXISTS( SELECT NULL FROM projects.COLUMNS WHERE table_name = 'projects')
+		THEN
+		ALTER TABLE projects ADD name varchar(255) NOT NULL;
+	END IF;
 	INSERT INTO projects (name) VALUES (project_name);
-	INSERT INTO correction (users_id, project_name, score) VALUES (users_id, project_name, score);
+	SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+	INSERT INTO corrections (users_id, project_id, score) VALUES (users_id, project_id, score);
 END
 $$
 DELIMiTER ;
