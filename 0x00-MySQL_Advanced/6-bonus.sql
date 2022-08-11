6ceabaefa5d94fb0614aba25c29f99572f0bca23
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-11 10:41:04 +0000

    creates a stored procedure AddBonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,9 +1,9 @@
 -- creates a stored procedure AddBonus that adds a new correction for student
-CREATE PROCEDURE AddBonus()
+CREATE PROCEDURE AddBonus(IN users_id INT, IN project_name VARCHAR(15), IN score FLOAT)
 BEGIN
-	INSERT INTO users (user_id) VALUES (NEW.users.id);
+	INSERT INTO users (id) VALUES (users_id);
 	CREATE TABLE IF NOT EXISTS projects;
-	ALTER TABLE projects ADD ROW IF NOT EXISTS projects_name;
-	INSERT INTO projects (project_name) VALUES (NEW.project_name);
-	INSERT INTO correction (score) VALUES (NEW.score);
-END;
+	ALTER TABLE projects ADD ROW IF NOT EXISTS name;
+	INSERT INTO projects (name) VALUES (project_name);
+	INSERT INTO correction (users.id, project_name, score) VALUES (users.id, project_name, score);
+END
