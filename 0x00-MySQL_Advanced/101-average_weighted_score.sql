ce641f138efe6f0398b3519b6466e582fe34f63d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-12 23:27:36 +0000

    Average weighted score for all students

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	UPDATE users AS usr, (SELECT ((SUM(score * weight)) / (SUM(weight))) AS wht FROM corrections AS cor JOIN projects AS pro ON cor.project_id = pro.id GROUP BY cor.user_id) AS scr SET average_score = wht WHERE usr.id = scr.id;
+	UPDATE users AS usr, (SELECT (((SUM(score * weight)) / (SUM(weight))) AS wht FROM corrections AS cor JOIN projects AS pro ON cor.project_id = pro.id GROUP BY cor.user_id), usr.id) AS scr SET average_score = wht WHERE usr.id = scr.id;
 END
 $$
 DELIMITER ;
