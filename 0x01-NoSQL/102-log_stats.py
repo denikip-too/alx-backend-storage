e89b0ab72aeb8f160b22c4cdb16a087853e4022b
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-16 22:49:01 +0000

    Log stats

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""Improve 12-log_stats.py by adding the top 10 of the most present IPs
+in the collection nginx of the database logs"""
---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+"""script that provides some stats about Nginx logs stored in MongoDB"""
