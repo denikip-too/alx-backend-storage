e81ceffd8bd209f88d34bcfdfb5214f4d17af23c
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-16 22:08:01 +0000

    returns all students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,4 +4,5 @@
 
 def top_students(mongo_collection):
     """function that returns all students sorted by average score"""
-    return (mongo_collection.aggregate({ "$project": { "name": "$name","averageScore": { "$avg": "$topics.score"} } }, { "$sort": { "averageScore": -1 } }))
+    scores = mongo_collection.aggregate({ "$project": { "name": "$name","averageScore": { "$avg": "$topics.score"} } }, { "$sort": { "averageScore": -1 } })
+    return
