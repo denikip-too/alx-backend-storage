f1517647d4dabe399cb0ff2aa069cdc2501194fd
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-16 20:06:11 +0000

    changes all topics of a school document based on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school document """
+    return (mongo_collection.update(name, topics))
