9d5cc5dc2eae406d315c5ad8ad41ebe2e7437c23
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-17 19:38:25 +0000

    python

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """Writing strings to Redis"""
 import redis
 import uuid
+from functools import wraps
 
 
 class Cache:
@@ -10,10 +11,24 @@ class Cache:
     def __init__(self):
         """store an instance of the Redis client as a private variable named
         _redis (using redis.Redis()) and flush the instance using flushdb"""
-        _redis = redis.Redis()
-        _redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: [str, bytes, int, float]) -> str:
         """method that takes a data argument and returns a string"""
         self.data = uuid.uuid1()
         return (str(self.data))
+
+    def get(self, key, fn = 'fn'):
+        """will be used to convert the data back to the desired format"""
+        if key is None:
+            return (self._redis)
+        return (self._redis)
+
+    def get_str(self):
+        """will automatically parametrize Cache.get to str"""
+        return (str(Cache.get))
+
+    def get_int(self):
+        """will automatically parametrize Cache.get to int"""
+        return (int(Cache.get))
